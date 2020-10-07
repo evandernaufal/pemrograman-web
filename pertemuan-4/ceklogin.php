95acb17b28ddf8e9ac832cd60759f3908fdd8551
evandernaufal <falevan17@gmail.com>
2020-10-07 21:21:14 +0700

    update session

--- pertemuan-4/ceklogin.php
+++ pertemuan-4/ceklogin.php
@@ -1,16 +1,20 @@
 <?php
+session_start();
     if (isset($_POST['tombolsubmit'])){
         $username = $_POST['username'];
         $password = $_POST['password'];
 
         if ($username == "anto" && $password == "123"){
-            echo "Sukses";
+            //echo "Sukses";
+            $_SESSION["sessionUsername"] = $username;
+            header("Location: dashboard.php");
         }
         else{
             echo "Username/password ada yang salah";
         }
     }
     else{
-        echo "Mohon maaf cek login tidak bisa diakses langsung";
+        header("Username/password ada yang salah")
+        //echo "Mohon maaf cek login tidak bisa diakses langsung";
     } 
 ?>
