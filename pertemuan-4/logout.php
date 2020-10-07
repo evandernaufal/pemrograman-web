f9c0895bcc0f33442d1437e64b7fc54c1dd9b268
evandernaufal <falevan17@gmail.com>
2020-10-07 21:39:20 +0700

    add logout.php

---
+++ pertemuan-4/logout.php
@@ -0,0 +1,9 @@
+<?php 
+    session_start();
+    session_unset();
+    session_destroy();
+    if (!isset($_SESSION["sessionUsername"])) {
+       header("Location: login.php");
+       //echo "session sudah didestroy";
+    }
+?>
